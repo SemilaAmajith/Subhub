a55a02db7b1409133ce8856175ef484ae3e8d38a
SemilaAmajith <139695343+SemilaAmajith@users.noreply.github.com>
2026-05-09 22:27:14 +0530

    Add files via upload

--- api/[...path].js
+++ api/[...path].js
@@ -1,8 +1,2 @@
-const path = require('path');
-
-// Tell Node.js to also look for packages inside the 'api' folder
-// This fixes the Vercel 500 error when backend/server.js tries to load express
-module.paths.push(path.join(__dirname, 'node_modules'));
-
 const app = require('../backend/server.js');
 module.exports = app;
